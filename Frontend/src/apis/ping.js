27169b7358f641a7176932c0f6dc751054cb78a2
Suhaib Khan <suhaib0900@gmail.com>
2026-01-16 17:12:55 +0000

    React Query Enabled

--- Frontend/src/apis/ping.js
+++ Frontend/src/apis/ping.js
@@ -2,7 +2,7 @@ import axiosInstance from "../config/axiosConfig";
 const pingApi = async () => {
   try {
     const res = await axiosInstance.get("/api/v1/ping");
-    if (res && res?.msg) console.log(res.msg);
+    if (res?.data?.msg) console.log(res.data.msg);
   } catch (err) {
     console.error(err);
   }
