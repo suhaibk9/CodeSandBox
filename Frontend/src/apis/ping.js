dbb91fc1a3031b6e904e55c60f1dc519e247e768
Suhaib Khan <suhaib0900@gmail.com>
2026-01-16 17:08:06 +0000

    API Sorted

--- Frontend/src/apis/ping.js
+++ Frontend/src/apis/ping.js
@@ -2,7 +2,7 @@ import axiosInstance from "../config/axiosConfig";
 const pingApi = async () => {
   try {
     const res = await axiosInstance.get("/api/v1/ping");
-    console.log(res["msg"]);
+    if (res && res?.msg) console.log(res.msg);
   } catch (err) {
     console.error(err);
   }
